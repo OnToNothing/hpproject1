4f136980042e5a6da81e02c696c9196f7007ab3a
Yousuf <ykanan2004@gmail.com>
2024-03-14 11:41:06 -0400

    no longer syntaxing errrs

--- nbody-s.c
+++ nbody-s.c
@@ -131,43 +131,50 @@ int main(int argc, const char* argv[]) {
     double accelerationX;
     double accelerationY;
     double accelerationZ;
-    for (size_t i = 0; i < num_steps; i++) {
-        if ( i % output_steps == 0) {
+    double massi;
+    double massj;
+    double xi;
+    double xj;
+    double yi;
+    double yj;
+    double zi;
+    double zj;
+    double euclideanDistance;
+    for (size_t t = 1; t < num_steps; t++) { 
+        for (size_t i = 0; i < n; i++) {
             for (size_t j = 0; j < n; j++) {
-                output->data[i*3*n+j*3] = input->data[j*7+1];
-                output->data[i*3*n+j*3+1] = input->data[j*7+2];
-                output->data[i*3*n+j*3+2] = input->data[j*7+3];
-            }
-        }
-        for (size_t j = 0; j < n; j++) {
-            double massi = input->data[j*7];
-            double massj = input->data[i*7]; 
-            double xi = input->data[i*7+1];
-            double xj = input->data[j*7+1];
-            double yi = input->data[i*7+2];
-            double yj = input->data[j*7+2];
-            double zi = input->data[i*7+3];
-            double zj = input->data[j*7+3];
-            double euclideanDistance = sqrt((xj - xi) * (xj - xi) + (yj - yi) * (yj - yi) + (zj - zi) * (zj - zi) + SOFTENING);
-            accelerationX = superpositionPrinciple(massi, massj,xj - xi, n, euclideanDistance);
-            accelerationY = superpositionPrinciple(massi, massj,yj - yi, n, euclideanDistance);
-            accelerationZ = superpositionPrinciple(massi, massj,zj - zi, n, euclideanDistance);
-            velocityx = computeVelocity(time_step, accelerationX, velocityx);
-            velocityy = computeVelocity(time_step, accelerationY, velocityy);
-            velocityz = computeVelocity(time_step, accelerationZ, velocityz);
-            inputCopy->data[j*7+1] += velocityx;
-            inputCopy->data[j*7+2] += velocityy;
-            inputCopy->data[j*7+3] += velocityz;
-        
-        
-            if (i % output_steps == 0) {
+                massi = input->data[j*7];
+                massj = input->data[i*7]; 
+                xi = input->data[i*7+1];
+                xj = input->data[j*7+1];
+                yi = input->data[i*7+2];
+                yj = input->data[j*7+2];
+                zi = input->data[i*7+3];
+                zj = input->data[j*7+3];
+                euclideanDistance = sqrt((xj - xi) * (xj - xi) + (yj - yi) * (yj - yi) + (zj - zi) * (zj - zi) + SOFTENING);
+                accelerationX = superpositionPrinciple(massi, massj,xj - xi, n, euclideanDistance);
+                accelerationY = superpositionPrinciple(massi, massj,yj - yi, n, euclideanDistance);
+                accelerationZ = superpositionPrinciple(massi, massj,zj - zi, n, euclideanDistance);
+                velocityx = computeVelocity(time_step, accelerationX, velocityx);
+                velocityy = computeVelocity(time_step, accelerationY, velocityy);
+                velocityz = computeVelocity(time_step, accelerationZ, velocityz);
+                inputCopy->data[j*7+1] += velocityx;
+                inputCopy->data[j*7+2] += velocityy;
+                inputCopy->data[j*7+3] += velocityz;
+
+                if (i % output_steps == 0) {
                 output->data[i*3*n+j*3] = inputCopy->data[j*7+1];
                 output->data[i*3*n+j*3+1] = inputCopy->data[j*7+2];
                 output->data[i*3*n+j*3+2] = inputCopy->data[j*7+3];
             }
-
+        
+        
+          
         }
     }
+    }
+        
+    
     if (num_steps % output_steps != 0) {
         //Save positions to reow "num_output-1" of output 
         for (size_t i = 0; i < n; i++) {
