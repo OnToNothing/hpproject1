e27840afa2d278c1b6dad7a704072caced76522e
Yousuf <ykanan2004@gmail.com>
2024-03-15 14:28:57 -0400

    fix most problems with nbody-s

--- nbody-s.c
+++ nbody-s.c
@@ -1,9 +1,9 @@
 /**
  * Runs a simulation of the n-body problem in 3D.
- * 
+ *
  * To compile the program:
  *   gcc -Wall -Ofast -march=native nbody-s.c matrix.c util.c -o nbody-s -lm
- * 
+ *
  * To run the program:
  *   ./nbody-s time-step total-time outputs-per-body input.npy output.npy
  * where:
@@ -12,19 +12,19 @@
  *   - outputs-per-body is the number of positions to output per body
  *   - input.npy is the file describing the initial state of the system (below)
  *   - output.npy is the output of the program (see below)
- * 
+ *
  * input.npy has a n-by-7 matrix with one row per body and the columns:
  *   - mass (in kg)
  *   - initial x, y, z position (in m)
  *   - initial x, y, z velocity (in m/s)
- * 
+ *
  * output.npy is generated and has a (outputs-per-body)-by-(3n) matrix with each
  * row containing the x, y, and z positions of each of the n bodies after a
  * given timestep.
- * 
+ *
  * See the PDF for implementation details and other requirements.
- * 
- * AUTHORS:Derek Allmon and yousuf kanan 
+ *
+ * AUTHORS:Derek Allmon and yousuf kanan
  */
 
 #include <stdbool.h>
@@ -42,55 +42,72 @@
 // Softening factor to reduce divide-by-near-zero effects
 #define SOFTENING 1e-9
 
-// double lawOfGravity(double massi , double massj , double xi , double xj , double yi , double yj 
+// double lawOfGravity(double massi , double massj , double xi , double xj , double yi , double yj
 //                     , double zi , double zj){
 //     double delta_x = xj - xi;
 //     double delta_y = yj - yi;
 //     double delta_z = zj - zi;
 //     double r = sqrt(delta_x*delta_x + delta_y*delta_y + delta_z*delta_z + SOFTENING);
-//     double rSquared = r*r; 
+//     double rSquared = r*r;
 //     double force = G * ((massi * massj) / rSquared);
 //     return force;
 // }
 
+double superpositionPrinciple(double massi, double massj, double delta, size_t n, double euclideanDistance)
+{
 
-double superpositionPrinciple(double massi, double massj,double delta ,size_t n, double euclideanDistance) {
-    
     const double distanceCubed = euclideanDistance * euclideanDistance * euclideanDistance;
-
-    // Calculate superpositions
-    double superposition = 0;
-
-    for (size_t j = 0; j <= n; ++j) {
-        superposition += massj * (delta) / (distanceCubed);
-    }
+    double superposition = massj * (delta) / (distanceCubed);
     superposition *= G; // Apply gravitational constant
     return superposition;
 }
 
+// run the simulation
 
-double computeVelocity(double time_step, double accelartion, double velocity) {
-  // Half-step acceleration update
-  return velocity + time_step * accelartion;
-}
-//run the simulation
-
-int main(int argc, const char* argv[]) {
+int main(int argc, const char *argv[])
+{
     // parse arguments
-    if (argc != 6 && argc != 7) { fprintf(stderr, "usage: %s time-step total-time outputs-per-body input.npy output.npy [num-threads]\n", argv[0]); return 1; }
+    if (argc != 6 && argc != 7)
+    {
+        fprintf(stderr, "usage: %s time-step total-time outputs-per-body input.npy output.npy [num-threads]\n", argv[0]);
+        return 1;
+    }
     double time_step = atof(argv[1]), total_time = atof(argv[2]);
-    if (time_step <= 0 || total_time <= 0 || time_step > total_time) { fprintf(stderr, "time-step and total-time must be positive with total-time > time-step\n"); return 1; }
+    if (time_step <= 0 || total_time <= 0 || time_step > total_time)
+    {
+        fprintf(stderr, "time-step and total-time must be positive with total-time > time-step\n");
+        return 1;
+    }
     size_t num_outputs = atoi(argv[3]);
-    if (num_outputs <= 0) { fprintf(stderr, "outputs-per-body must be positive\n"); return 1; }
-    Matrix* input = matrix_from_npy_path(argv[4]);
-    if (input == NULL) { perror("error reading input"); return 1; }
-    if (input->cols != 7) { fprintf(stderr, "input.npy must have 7 columns\n"); return 1; }
+    if (num_outputs <= 0)
+    {
+        fprintf(stderr, "outputs-per-body must be positive\n");
+        return 1;
+    }
+    Matrix *input = matrix_from_npy_path(argv[4]);
+    if (input == NULL)
+    {
+        perror("error reading input");
+        return 1;
+    }
+    if (input->cols != 7)
+    {
+        fprintf(stderr, "input.npy must have 7 columns\n");
+        return 1;
+    }
     size_t n = input->rows;
-    if (n == 0) { fprintf(stderr, "input.npy must have at least 1 row\n"); return 1; }
+    if (n == 0)
+    {
+        fprintf(stderr, "input.npy must have at least 1 row\n");
+        return 1;
+    }
     size_t num_steps = (size_t)(total_time / time_step + 0.5);
-    if (num_steps < num_outputs) { num_outputs = 1; }
-    size_t output_steps = num_steps/num_outputs;
-    num_outputs = (num_steps+output_steps-1)/output_steps;
+    if (num_steps < num_outputs)
+    {
+        num_outputs = 1;
+    }
+    size_t output_steps = num_steps / num_outputs;
+    num_outputs = (num_steps + output_steps - 1) / output_steps;
 
     // variables available now:
     //   time_step    number of seconds between each time point
@@ -106,87 +123,85 @@ int main(int argc, const char* argv[]) {
     clock_gettime(CLOCK_MONOTONIC, &start);
 
     // allocate output matrix as a 3n-by-num_outputs matrix
-    Matrix* output = matrix_create_raw(num_outputs, 3*n);
+    Matrix *output = matrix_create_raw(num_outputs, 3 * n);
 
-    //make row 0 the iniitial position
-    for (size_t i = 0; i < n; i++) {
-        output->data[i*3] = input->data[i*7+1];
-        output->data[i*3+1] = input->data[i*7+2];
-        output->data[i*3+2] = input->data[i*7+3];
+    // make row 0 the iniitial position
+    for (size_t i = 0; i < n; i++)
+    {
+        output->data[i * 3] = input->data[i * 7 + 1];
+        output->data[i * 3 + 1] = input->data[i * 7 + 2];
+        output->data[i * 3 + 2] = input->data[i * 7 + 3];
+        
     }
-    Matrix* inputCopy = matrix_copy(input); 
-    //run the simulation
-    //MAKE INPUT DATA USABLE BY COPYING IT TO A NEW ARRAY
-    
-    //HAVE INDEXED I AND J LOOPS TO CALL SUPERPOSITION PRINCIPLE
-    //CALL SUPERPOSITION PRINCIPLE TO GET THE FORCE
-    //CALL COMPUTE VELOCITY TO GET THE VELOCITY
-    //UPDATE THE POSITION OF EACH BODY
-    //PERIODICALLY COPY THE POSITION TO THE OUTPUT MATRIX
-    //if i == j then skip the iteration
-    //FREE THE MEMORY
-    double velocityx = input->data[4];
-    double velocityy = input->data[5];
-    double velocityz = input->data[6];
-    double accelerationX;
-    double accelerationY;
-    double accelerationZ;
-    double massi;
-    double massj;
-    double xi;
-    double xj;
-    double yi;
-    double yj;
-    double zi;
-    double zj;
-    double euclideanDistance;
-    for (size_t t = 1; t < num_steps; t++) { 
-        for (size_t i = 0; i < n; i++) {
-            for (size_t j = 0; j < n; j++) {
-                massi = input->data[j*7];
-                massj = input->data[i*7]; 
-                xi = input->data[i*7+1];
-                xj = input->data[j*7+1];
-                yi = input->data[i*7+2];
-                yj = input->data[j*7+2];
-                zi = input->data[i*7+3];
-                zj = input->data[j*7+3];
-                euclideanDistance = sqrt((xj - xi) * (xj - xi) + (yj - yi) * (yj - yi) + (zj - zi) * (zj - zi) + SOFTENING);
-                accelerationX = superpositionPrinciple(massi, massj,xj - xi, n, euclideanDistance);
-                accelerationY = superpositionPrinciple(massi, massj,yj - yi, n, euclideanDistance);
-                accelerationZ = superpositionPrinciple(massi, massj,zj - zi, n, euclideanDistance);
-                velocityx = computeVelocity(time_step, accelerationX, velocityx);
-                velocityy = computeVelocity(time_step, accelerationY, velocityy);
-                velocityz = computeVelocity(time_step, accelerationZ, velocityz);
-                inputCopy->data[j*7+1] += velocityx;
-                inputCopy->data[j*7+2] += velocityy;
-                inputCopy->data[j*7+3] += velocityz;
+    Matrix *inputCopy = matrix_copy(input);
+    // run the simulation
+    // MAKE INPUT DATA USABLE BY COPYING IT TO A NEW ARRAY
 
-                if (i % output_steps == 0) {
-                output->data[i*3*n+j*3] = inputCopy->data[j*7+1];
-                output->data[i*3*n+j*3+1] = inputCopy->data[j*7+2];
-                output->data[i*3*n+j*3+2] = inputCopy->data[j*7+3];
+    // HAVE INDEXED I AND J LOOPS TO CALL SUPERPOSITION PRINCIPLE
+    // CALL SUPERPOSITION PRINCIPLE TO GET THE FORCE
+    // CALL COMPUTE VELOCITY TO GET THE VELOCITY
+    // UPDATE THE POSITION OF EACH BODY
+    // PERIODICALLY COPY THE POSITION TO THE OUTPUT MATRIX
+    // if i == j then skip the iteration
+    // FREE THE MEMORY
+    for (size_t t = 1; t < num_steps; t++)
+    {
+        for (size_t i = 0; i < n; i++)
+        {
+            double xi = inputCopy->data[i * 7 + 1];
+            double yi = inputCopy->data[i * 7 + 2];
+            double zi = inputCopy->data[i * 7 + 3];
+            double accelerationX = 0;
+            double accelerationY = 0;
+            double accelerationZ = 0;
+            for (size_t j = 0; j < n; j++)
+            {
+                double massi = inputCopy->data[i * 7];
+                double massj = inputCopy->data[j * 7];
+
+                double xj = inputCopy->data[j * 7 + 1];
+                double yj = inputCopy->data[j * 7 + 2];
+                double zj = inputCopy->data[j * 7 + 3];
+                double euclideanDistance = sqrt((xj - xi) * (xj - xi) + (yj - yi) * (yj - yi) + (zj - zi) * (zj - zi) + SOFTENING);
+                const double distanceCubed = euclideanDistance * euclideanDistance * euclideanDistance;
+                double factor = G * massj / distanceCubed;
+                accelerationX += factor * (xj - xi);
+                accelerationY += factor * (yj - yi);
+                accelerationZ += factor * (zj - zi);
             }
-        
-        
-          
+            inputCopy->data[i * 7 + 4] += accelerationX * time_step;
+            inputCopy->data[i * 7 + 5] += accelerationY * time_step;
+            inputCopy->data[i * 7 + 6] += accelerationZ * time_step;
+            inputCopy->data[i * 7 + 1] += inputCopy->data[i * 7 + 4] * time_step;
+            inputCopy->data[i * 7 + 2] += inputCopy->data[i * 7 + 5] * time_step;
+            inputCopy->data[i * 7 + 3] += inputCopy->data[i * 7 + 6] * time_step;
         }
+        if (t % output_steps == 0)
+                {
+                    // TODO: save positions to row `t/output_steps` of output
+                    for (size_t i = 0; i < n; i++)
+                    {
+                        output->data[(t / output_steps) * 3 * n + i * 3] = inputCopy->data[i * 7 + 1];
+                        output->data[(t / output_steps) * 3 * n + i * 3 + 1] = inputCopy->data[i * 7 + 2];
+                        output->data[(t / output_steps) * 3 * n + i * 3 + 2] = inputCopy->data[i * 7 + 3];
+
+                        
+                    }
+
+                }
     }
-    }
-        
-    
-    if (num_steps % output_steps != 0) {
-        //Save positions to reow "num_output-1" of output 
-        for (size_t i = 0; i < n; i++) {
-            output->data[(num_outputs-1)*3*n+i*3] = input->data[i*7+1];
-            output->data[(num_outputs-1)*3*n+i*3+1] = input->data[i*7+2];
-            output->data[(num_outputs-1)*3*n+i*3+2] = input->data[i*7+3];
+
+    if (num_steps % output_steps == 0)
+    {
+        // Save positions to reow "num_output-1" of output
+        for (size_t i = 0; i < n; i++)
+        {
+            output->data[(num_outputs - 1) * 3 * n + i * 3] = inputCopy->data[i * 7 + 1];
+            output->data[(num_outputs - 1) * 3 * n + i * 3 + 1] = inputCopy->data[i * 7 + 2];
+            output->data[(num_outputs - 1) * 3 * n + i * 3 + 2] = inputCopy->data[i * 7 + 3];
         }
     }
 
-
-        
-    
     // get the end and computation time
     clock_gettime(CLOCK_MONOTONIC, &end);
     double time = get_time_diff(&start, &end);
@@ -199,7 +214,6 @@ int main(int argc, const char* argv[]) {
     matrix_free(input);
     matrix_free(inputCopy);
     matrix_free(output);
-
 
     return 0;
 }
